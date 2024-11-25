bf1c35ed0f352c3e35da91cd22d60aa119447a5d
AslamNazeerShaikh <Aslam.Nazeer.Shaikh@outlook.in>
2024-11-25 23:56:42 +0530

    Deleted WebAPI & Added MVC Project

---
+++ RepositoryPattern.WebUi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RepositoryPattern.WebUi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

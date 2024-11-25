7ed91e8e70a8b66b26409ed9fa80d82c5c1ef120
AslamNazeerShaikh <Aslam.Nazeer.Shaikh@outlook.in>
2024-11-26 02:05:50 +0530

    Added CRUD Views & Custom Font Face

--- RepositoryPattern.WebUi/Controllers/ItemsController.cs
+++ RepositoryPattern.WebUi/Controllers/ItemsController.cs
@@ -18,17 +18,32 @@ namespace RepositoryPattern.WebUi.Controllers
             _repository = repository;
         }
 
-        [HttpGet("index")]
+        // GET: Items
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var items = await _repository.GetAllAsync();
             return View(items);
         }
 
-        [HttpGet("create")]
-        public IActionResult Create() => View();
+        // GET: Items/Details/{id}
+        [HttpGet("Details/{id}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            var item = await _repository.GetByIdAsync(id);
+            if (item == null) return NotFound();
+            return View(item);
+        }
 
-        [HttpPost("create")]
+        // GET: Items/Create
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Items/Create
+        [HttpPost("Create")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Item item)
         {
@@ -40,12 +55,46 @@ namespace RepositoryPattern.WebUi.Controllers
             return View(item);
         }
 
-        // Implement Edit and Delete actions similarly...
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        // GET: Items/Edit/{id}
+        [HttpGet("Edit/{id}")]
+        public async Task<IActionResult> Edit(int id)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var item = await _repository.GetByIdAsync(id);
+            if (item == null) return NotFound();
+            return View(item);
+        }
+
+        // POST: Items/Edit/{id}
+        [HttpPost("Edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Item item)
+        {
+            if (id != item.Id) return BadRequest();
+
+            if (ModelState.IsValid)
+            {
+                await _repository.UpdateAsync(item);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+
+        // GET: Items/Delete/{id}
+        [HttpGet("Delete/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var item = await _repository.GetByIdAsync(id);
+            if (item == null) return NotFound();
+            return View(item);
+        }
+
+        // POST: Items/Delete/{id}
+        [HttpPost("Delete/{id}"), ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _repository.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
